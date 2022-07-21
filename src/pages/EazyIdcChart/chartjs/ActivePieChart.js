7509f9452b73c9ea2248f9aad348b29e3e9a4308
Atcharawan B <80392458+MiiKiiza@users.noreply.github.com>
2022-07-21 15:24:25 +0700

    add pie chart

---
+++ src/pages/EazyIdcChart/chartjs/ActivePieChart.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { Pie } from "react-chartjs-2";
+
+class ActivePieChart extends Component {
+  render() {
+    const data = {
+      labels: [
+        "Active",
+        "Inactive"
+      ]
+    };
+
+    const option = {
+      tooltips: {
+        callbacks: {
+          label: function (tooltipItem, data) {
+            var dataset = data.datasets[tooltipItem.datasetIndex];
+            var meta = dataset._meta[Object.keys(dataset._meta)[0]];
+            var total = meta.total;
+            var currentValue = dataset.data[tooltipItem.index];
+            var percentage = parseFloat((currentValue / total * 100).toFixed(1));
+            return currentValue + ' (' + percentage + '%)';
+          },
+          title: function (tooltipItem, data) {
+            return data.labels[tooltipItem[0].index]
+          }
+        }
+      }
+    }
+
+    return (
+      <React.Fragment>
+        <Pie width={600} height={215} data={data} options={option} />
+      </React.Fragment>
+    )
+  }
+}
+
+export default ActivePieChart;
